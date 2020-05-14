c4fc8ccf6e56c5f143960a5873debc9428639bb2
oli <oli.shubham@gmail.com>
2020-05-14 21:18:57 +0530

    Done: LC#208

---
+++ leetcode/medium/208-Implement_trie.py
@@ -0,0 +1,65 @@
+"""
+    Leetcode #208
+"""
+
+
+class Node:
+
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for i in word:
+            if i not in node.children:
+                node.children[i] = Node()
+            node = node.children[i]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for i in word:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return node.word
+
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for i in prefix:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return True
+
+
+if __name__ == "__main__":
+
+    trie = Trie()
+
+    trie.insert("apple");
+    assert trie.search("apple") == True
+    assert trie.search("app") == False
+    assert trie.startsWith("app") == True
+
+    trie.insert("app")
+    assert trie.search("app") == True
